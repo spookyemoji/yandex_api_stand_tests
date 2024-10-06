bdb8ff0a0b03f8f53d7af3a6cdb3f1538e4b48dd
spookyemoji <artemiy.skl@gmail.com>
2024-10-06 21:19:46 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://595039e5-7fc3-4454-b3c7-b13bd2dde407.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
